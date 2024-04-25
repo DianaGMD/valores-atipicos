8201d0a00b8b9df9a178d0b941375be7dcbdbbd0
DianaGMD <a01368899@tec.mx>
2024-04-24 18:23:12 -0600

    graficas

--- outliers.py
+++ outliers.py
@@ -4,4 +4,23 @@ import matplotlib.pyplot as plt
 
 print('Hello outliers0')
 df= pd.read_csv('Ventas_totales_sinnulos.csv', index_col=0)
-print(df.head())
+#print(df.head()) 
+
+valores_nulos=df.isnull().sum()
+valores_nulos
+
+fig = plt.figure(figsize =(7, 3))
+plt.hist(x=df["ventas_precios_corrientes"], color='blue', rwidth=0.50)
+plt.title('Histograma de ventas_precios_corrientes')
+plt.xlabel('ventas_precios_corrientes')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+fig = plt.figure(figsize =(5, 3))
+plt.boxplot(df["ventas_precios_corrientes"]) 
+plt.title("Outliers de ventas_precios_corriente")
+#plt.show()
+
+#Método aplicando Cuartiles. Encuentro cuartiles 0.25 y 0.75
+y=df["ventas_precios_corrientes"]
+print(y)
